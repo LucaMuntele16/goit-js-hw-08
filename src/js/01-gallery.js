2c0b0fb0832e74b4b1247d89c955a94560ff72f5
LucaMuntele16 <119846365+LucaMuntele16@users.noreply.github.com>
2024-05-21 17:12:01 +0300

    mj

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
+
+const galleryStart = document.querySelector(".gallery");
+
+function createGallery() {
+  let galleryHTML = "";
+
+  for (let i = 0; i < galleryItems.length; i++) {
+    galleryHTML += `
+    <li class="gallery__item">
+    <a class="gallery__link" href="${galleryItems[i].original}" onclick="event.preventDefault()">
+    <img
+    class="gallery__image"
+    src="${galleryItems[i].preview}"
+    alt="${galleryItems[i].description}"
+    />
+    </a>
+    </li>
+    `;
+  }
+  galleryStart.innerHTML = galleryHTML;
+}
+createGallery();
+  var lightbox = new SimpleLightbox('.gallery a', {captionsData : 'alt', captionDelay:250 });
 
 console.log(galleryItems);
